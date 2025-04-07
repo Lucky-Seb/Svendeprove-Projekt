ad079a7c0ebe446c373654e1720e73010b759472
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-07 15:25:44 +0200

    added Spørgsmål and Teknik model

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Models/Spørgsmål.cs
@@ -0,0 +1,20 @@
+﻿namespace TaekwondoOrchestration.ApiService.Models
+{
+    public class Spørgsmål
+    {
+        public int SpørgsmålID { get; set; }
+        public int SpørgsmålRækkefølge { get; set; }
+        public int SpørgsmålTid { get; set; }
+
+        //ForeigenKey
+        public int? TeoriID { get; set; }
+        public int? TeknikID { get; set; }
+        public int? ØvelseID { get; set; }
+        public int QuizID { get; set; }
+
+        public Quiz? Quiz { get; set; }
+        public Teori? Teori { get; set; }
+        public Teknik? Teknik { get; set; }
+        public Øvelse? Øvelse { get; set; }
+    }
+}
