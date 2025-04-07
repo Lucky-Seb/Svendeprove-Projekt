224d355ea1d6a0129ab337e0fac6d841cc455fa3
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-07 15:23:46 +0200

    add pensum and øvelse

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Models/Øvelse.cs
@@ -0,0 +1,18 @@
+﻿namespace TaekwondoOrchestration.ApiService.Models
+{
+    public class Øvelse
+    {
+        public int ØvelseID { get; set; }
+        public string ØvelseNavn { get; set; }
+        public string ØvelseBeskrivelse { get; set; }
+        public string ØvelseBillede { get; set; }
+        public string ØvelseVideo { get; set; }
+        public int ØvelseTid { get; set; }
+        public string ØvelseSværhed { get; set; }
+
+        public int PensumID { get; set; }  // Foreign Key
+
+        public ICollection<BrugerØvelse>? BrugerØvelses { get; set; }
+        public ICollection<KlubØvelse>? KlubØvelses { get; set; }
+    }
+}
