6c74f33fe9198af217645298b5cdec225383e744
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-07 13:58:58 +0200

    testing with adding server side maui web to aspire

--- TaekwondoApp/TaekwondoApp.Web/Program.cs
+++ TaekwondoApp/TaekwondoApp.Web/Program.cs
@@ -4,6 +4,11 @@ using TaekwondoApp.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Add service defaults & Aspire client integrations.
+builder.AddServiceDefaults();
+builder.AddRedisOutputCache("cache");
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
--- TaekwondoOrchestration/TaekwondoOrchestration.AppHost/Program.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.AppHost/Program.cs
@@ -4,11 +4,19 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.TaekwondoOrchestration_ApiService>("apiservice");
 
-builder.AddProject<Projects.TaekwondoOrchestration_Web>("webfrontend")
+builder.AddProject<Projects.TaekwondoOrchestration_Web>("webWeafrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WaitFor(cache)
     .WithReference(apiService)
     .WaitFor(apiService);
+
+builder.AddProject<Projects.TaekwondoApp_Web>("webTaefrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
 
 builder.Build().Run();
