8fb1e67dcb343273ab48366080862ffff92ae7e7
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-07 15:31:53 +0200

    Added Bruger and klub DTOS

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/DTO/BrugerDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace TaekwondoOrchestration.ApiService.DTO
+{
+    public class BrugerDTO
+    {
+        public int BrugerID { get; set; }
+        public string Email { get; set; }
+        public string Brugernavn { get; set; }
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string Brugerkode { get; set; } // This should be hashed in your logic
+        public string Bæltegrad { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+        public KlubDTO Klub { get; set; }
+    }
+}
---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/DTO/KlubDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TaekwondoOrchestration.ApiService.DTO
+{
+    public class KlubDTO
+    {
+        public int KlubID { get; set; }
+        public string KlubNavn { get; set; }
+    }
+}
