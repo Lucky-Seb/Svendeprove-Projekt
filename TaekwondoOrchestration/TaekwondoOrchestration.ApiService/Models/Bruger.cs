b6095d091647a6fa5fbc7b68ede861d07ffc2bf8
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-07 15:22:03 +0200

    Add bruger

--- TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Models/Bruger.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Models/Bruger.cs
@@ -2,5 +2,19 @@
 {
     public class Bruger
     {
+        public int BrugerID { get; set; }
+        public string Email { get; set; }
+        public string Brugernavn { get; set; }
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string Brugerkode { get; set; } // This should be hashed in your logic
+        public string Address { get; set; }
+        public string Bæltegrad { get; set; }
+        public string Role { get; set; }
+
+        public ICollection<BrugerKlub>? BrugerKlubber { get; set; }
+        public ICollection<BrugerProgram>? BrugerProgrammer { get; set; }
+        public ICollection<BrugerQuiz>? BrugerQuizzer { get; set; }
+        public ICollection<BrugerØvelse>? BrugerØvelser { get; set; }
     }
 }
