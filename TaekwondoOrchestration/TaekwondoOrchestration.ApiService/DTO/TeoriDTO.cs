b2dc6ce5dab055e06eec66b459e044cefcd57656
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-07 15:36:36 +0200

    added the rest of dtos

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/DTO/TeoriDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace TaekwondoOrchestration.ApiService.DTO
+{
+    public class TeoriDTO
+    {
+        public int TeoriID { get; set; }
+        public string TeoriNavn { get; set; }
+        public string TeoriBeskrivelse { get; set; }
+        public string TeoriBillede { get; set; }
+        public string TeoriVideo { get; set; }
+        public string TeoriLyd { get; set; }
+        public int PensumID { get; set; } // Foreign key
+    }
+}
