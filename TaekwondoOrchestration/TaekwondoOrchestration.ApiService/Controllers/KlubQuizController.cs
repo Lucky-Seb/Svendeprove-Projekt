3675c62dbc278f49c25cf1a2edffe3e63d658240
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:39:12 +0200

    Added KlubQuizController

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Controllers/KlubQuizController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaekwondoOrchestration.ApiService.Services;
+using TaekwondoOrchestration.ApiService.DTO;
+
+
+namespace TaekwondoOrchestration.ApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KlubQuizController : ControllerBase
+    {
+        private readonly KlubQuizService _klubQuizService;
+
+        public KlubQuizController(KlubQuizService klubQuizService)
+        {
+            _klubQuizService = klubQuizService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<KlubQuizDTO>>> GetKlubQuizzer()
+        {
+            return Ok(await _klubQuizService.GetAllKlubQuizzerAsync());
+        }
+
+        [HttpGet("{klubId}/{quizId}")]
+        public async Task<ActionResult<KlubQuizDTO>> GetKlubQuiz(int klubId, int quizId)
+        {
+            var klubQuiz = await _klubQuizService.GetKlubQuizByIdAsync(klubId, quizId);
+            if (klubQuiz == null)
+                return NotFound();
+            return Ok(klubQuiz);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<KlubQuizDTO>> PostKlubQuiz(KlubQuizDTO klubQuizDto)
+        {
+            var createdKlubQuiz = await _klubQuizService.CreateKlubQuizAsync(klubQuizDto);
+            return CreatedAtAction(nameof(GetKlubQuiz), new { klubId = createdKlubQuiz.KlubID, quizId = createdKlubQuiz.QuizID }, createdKlubQuiz);
+        }
+
+        [HttpDelete("{klubId}/{quizId}")]
+        public async Task<IActionResult> DeleteKlubQuiz(int klubId, int quizId)
+        {
+            var success = await _klubQuizService.DeleteKlubQuizAsync(klubId, quizId);
+            if (!success)
+                return NotFound();
+            return NoContent();
+        }
+    }
+}
