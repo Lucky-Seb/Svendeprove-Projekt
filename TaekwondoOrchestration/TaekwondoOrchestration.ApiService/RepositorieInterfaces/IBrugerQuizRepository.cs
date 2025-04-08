e45ca71b64ea814e38be1f8b23559b20bd5a941b
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:20:36 +0200

    Added IBrugerQuizRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerQuizRepository.cs
@@ -0,0 +1,15 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IBrugerQuizRepository
+    {
+        Task<List<BrugerQuiz>> GetAllBrugerQuizzesAsync();
+        Task<BrugerQuiz?> GetBrugerQuizByIdAsync(int brugerId, int quizId);
+        Task<BrugerQuiz> CreateBrugerQuizAsync(BrugerQuiz brugerQuiz);
+        Task<bool> DeleteBrugerQuizAsync(int brugerId, int quizId);
+    }
+}
+
