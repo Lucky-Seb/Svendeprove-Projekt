331497eb27f1ec6c169f1d547818929cde78a2d3
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:18:36 +0200

    Added IbrugerRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerKlubRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public interface IBrugerKlubRepository
+    {
+        Task<List<BrugerKlub>> GetAllBrugerKlubberAsync();
+        Task<BrugerKlub?> GetBrugerKlubByIdAsync(int brugerId, int klubId);
+        Task<BrugerKlub> CreateBrugerKlubAsync(BrugerKlub brugerKlub);
+        Task<bool> DeleteBrugerKlubAsync(int brugerId, int klubId);
+    }
+}
