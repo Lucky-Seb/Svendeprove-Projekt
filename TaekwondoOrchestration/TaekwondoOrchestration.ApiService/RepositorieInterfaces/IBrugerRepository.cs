24fbb48c765f645d45bcd60557f2d927331e6e75
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:20:50 +0200

    Added IbrugerRepository

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerRepository.cs
@@ -0,0 +1,25 @@
+﻿using TaekwondoOrchestration.ApiService.DTOs;
+using TaekwondoOrchestration.ApiService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IBrugerRepository
+    {
+        // Existing methods
+        Task<List<Bruger>> GetAllBrugereAsync();
+        Task<Bruger?> GetBrugerByIdAsync(int id);
+        Task<List<Bruger>> GetBrugerByRoleAsync(string role);
+        Task<List<Bruger>> GetBrugerByBælteAsync(string bæltegrad);
+        Task<List<BrugerDTO>> GetBrugereByKlubAsync(int klubId);
+        Task<List<BrugerDTO>> GetBrugereByKlubAndBæltegradAsync(int klubId, string bæltegrad);
+        Task<Bruger?> GetBrugerByBrugernavnAsync(string brugernavn);
+        Task<List<Bruger>> GetBrugerByFornavnEfternavnAsync(string fornavn, string efternavn);
+        Task<Bruger> CreateBrugerAsync(Bruger bruger);
+        Task<bool> UpdateBrugerAsync(Bruger bruger);
+
+        // Add DeleteBrugerAsync method
+        Task<bool> DeleteBrugerAsync(int id);
+    }
+}
