06f5c6e4a347dcc2a5b30f5f2fd646b264fd63e4
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:22:54 +0200

    Added IPensumRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IPensumRepository.cs
@@ -0,0 +1,15 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IPensumRepository
+    {
+        Task<List<Pensum>> GetAllAsync();
+        Task<Pensum?> GetByIdAsync(int id);
+        Task<Pensum> CreateAsync(Pensum pensum);
+        Task<bool> UpdateAsync(Pensum pensum);
+        Task<bool> DeleteAsync(int id);
+    }
+}
