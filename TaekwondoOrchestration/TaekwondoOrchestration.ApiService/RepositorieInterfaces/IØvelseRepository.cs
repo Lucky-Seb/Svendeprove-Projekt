60eb3f830fab4f890144e7e1ecd8b6e7fb003d8c
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:22:39 +0200

    Added IØvelseRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IØvelseRepository.cs
@@ -0,0 +1,17 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IØvelseRepository
+    {
+        Task<List<Øvelse>> GetAllØvelserAsync();
+        Task<Øvelse?> GetØvelseByIdAsync(int id);
+        Task<List<Øvelse>> GetØvelserBySværhedAsync(string sværhed);
+        Task<List<Øvelse>> GetØvelserByBrugerAsync(int brugerId);
+        Task<List<Øvelse>> GetØvelserByKlubAsync(int klubId);
+        Task<List<Øvelse>> GetØvelserByNavnAsync(string navn);
+        Task<Øvelse> CreateØvelseAsync(Øvelse øvelse);
+        Task<bool> DeleteØvelseAsync(int id);
+        Task<bool> UpdateØvelseAsync(Øvelse øvelse);
+    }
+}
