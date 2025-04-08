5eeabd426ad8fc94ec9ad06312579f83e88704f3
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:21:48 +0200

    Added IKlubQuizRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IKlubQuizRepository.cs
@@ -0,0 +1,12 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IKlubQuizRepository
+    {
+        Task<List<KlubQuiz>> GetAllKlubQuizzerAsync();
+        Task<KlubQuiz?> GetKlubQuizByIdAsync(int klubId, int quizId);
+        Task<KlubQuiz> CreateKlubQuizAsync(KlubQuiz klubQuiz);
+        Task<bool> DeleteKlubQuizAsync(int klubId, int quizId);
+    }
+}
