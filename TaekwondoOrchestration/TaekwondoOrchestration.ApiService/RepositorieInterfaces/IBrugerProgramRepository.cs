f0c592c0b5f454aa13aada6337bd0f581c4efb7c
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:20:20 +0200

    Added IBrugerProgramRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerProgramRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IBrugerProgramRepository
+    {
+        Task<List<BrugerProgram>> GetAllBrugerProgramsAsync();
+        Task<BrugerProgram?> GetBrugerProgramByIdAsync(int brugerId, int programId);
+        Task<BrugerProgram> CreateBrugerProgramAsync(BrugerProgram brugerProgram);
+        Task<bool> DeleteBrugerProgramAsync(int brugerId, int programId);
+    }
+}
