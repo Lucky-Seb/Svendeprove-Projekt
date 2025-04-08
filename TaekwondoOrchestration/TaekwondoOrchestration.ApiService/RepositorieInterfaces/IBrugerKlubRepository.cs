3d58ee9c873929948fc92e19c6f959fa1f62d412
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:20:01 +0200

    Added IBrugerØvelse

--- TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerKlubRepository.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerKlubRepository.cs
@@ -1,4 +1,4 @@
-﻿using Api.Models;
+﻿using TaekwondoOrchestration.ApiService.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IBrugerØvelseRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public interface IBrugerØvelseRepository
+    {
+        Task<List<BrugerØvelse>> GetAllBrugerØvelserAsync();
+        Task<BrugerØvelse?> GetBrugerØvelseByIdAsync(int brugerId, int øvelseId);
+        Task<BrugerØvelse> CreateBrugerØvelseAsync(BrugerØvelse brugerØvelse);
+        Task<bool> DeleteBrugerØvelseAsync(int brugerId, int øvelseId);
+    }
+}
