e92941508b901ba5a6b80061c297fa682fa5aa87
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:39:39 +0200

    Added KlubØvelseCOntroller

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Controllers/KlubØvelseController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaekwondoOrchestration.ApiService.Services;
+using TaekwondoOrchestration.ApiService.DTO;
+
+
+namespace TaekwondoOrchestration.ApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KlubØvelseController : ControllerBase
+    {
+        private readonly KlubØvelseService _klubØvelseService;
+
+        public KlubØvelseController(KlubØvelseService klubØvelseService)
+        {
+            _klubØvelseService = klubØvelseService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<KlubØvelseDTO>>> GetKlubØvelser()
+        {
+            return Ok(await _klubØvelseService.GetAllKlubØvelserAsync());
+        }
+
+        [HttpGet("{klubId}/{øvelseId}")]
+        public async Task<ActionResult<KlubØvelseDTO>> GetKlubØvelse(int klubId, int øvelseId)
+        {
+            var klubØvelse = await _klubØvelseService.GetKlubØvelseByIdAsync(klubId, øvelseId);
+            if (klubØvelse == null)
+                return NotFound();
+            return Ok(klubØvelse);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<KlubØvelseDTO>> PostKlubØvelse(KlubØvelseDTO klubØvelseDto)
+        {
+            var createdKlubØvelse = await _klubØvelseService.CreateKlubØvelseAsync(klubØvelseDto);
+            return CreatedAtAction(nameof(GetKlubØvelse), new { klubId = createdKlubØvelse.KlubID, øvelseId = createdKlubØvelse.ØvelseID }, createdKlubØvelse);
+        }
+
+        [HttpDelete("{klubId}/{øvelseId}")]
+        public async Task<IActionResult> DeleteKlubØvelse(int klubId, int øvelseId)
+        {
+            var success = await _klubØvelseService.DeleteKlubØvelseAsync(klubId, øvelseId);
+            if (!success)
+                return NotFound();
+            return NoContent();
+        }
+    }
+}
