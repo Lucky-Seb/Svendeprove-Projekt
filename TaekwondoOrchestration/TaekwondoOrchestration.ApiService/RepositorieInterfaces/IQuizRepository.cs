b89eb9861c4fa05da68b661e94d01ccc9390d903
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:23:28 +0200

    IQuizRepos

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/IQuizRepository.cs
@@ -0,0 +1,18 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface IQuizRepository
+    {
+        Task<List<Quiz>> GetAllAsync();
+        Task<Quiz?> GetByIdAsync(int id);
+        Task<Quiz> CreateAsync(Quiz quiz);
+        Task<bool> UpdateAsync(Quiz quiz);
+        Task<bool> DeleteAsync(int id);
+        Task<List<Quiz>> GetAllByBrugerAsync(int brugerId);
+        Task<List<Quiz>> GetAllByKlubAsync(int klubId);
+        Task<List<Quiz>> GetAllByPensumAsync(int pensumId);
+    }
+}
