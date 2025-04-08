37b4dc59ccec0111f22476b05f2fbe46fdf7ba76
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-08 14:24:38 +0200

    Added ItræningRepo

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/RepositorieInterfaces/ITræningRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaekwondoOrchestration.ApiService.Models;
+
+namespace TaekwondoOrchestration.ApiService.Repositories
+{
+    public interface ITræningRepository
+    {
+        Task<IEnumerable<Træning>> GetByProgramIdAsync(int id);
+        Task<List<Træning>> GetAllTræningAsync();
+        Task<Træning> GetTræningByIdAsync(int id);
+        Task<Træning> CreateTræningAsync(Træning træning);
+        Task<bool> DeleteTræningAsync(int id);
+        Task<bool> UpdateTræningAsync(Træning træning);
+    }
+}
