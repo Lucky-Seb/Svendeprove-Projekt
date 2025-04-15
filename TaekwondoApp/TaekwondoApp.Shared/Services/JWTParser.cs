377a72b89ce4db32f9c96235ff6f519ddf6350de
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-15 09:37:51 +0200

    create a helper class to extrat the role from jwt token

--- TaekwondoApp/TaekwondoApp.Shared/Services/JWTParser.cs
+++ TaekwondoApp/TaekwondoApp.Shared/Services/JWTParser.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace TaekwondoApp.Services
+namespace TaekwondoApp.Shared.Services
 {
-    internal class JWTParser
+    public static class JwtParser
     {
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(jwt);
+
+            return token.Claims;
+        }
+
+        public static string? GetRole(string jwt)
+        {
+            var claims = ParseClaimsFromJwt(jwt);
+            return claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+        }
     }
 }
