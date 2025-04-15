840594a7db041624612cc30659e575bf4127df85
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-15 16:18:28 +0200

    Added a base for controllers

--- TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Controllers/ApiBaseController.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Controllers/ApiBaseController.cs
@@ -1,6 +1,18 @@
-﻿namespace TaekwondoOrchestration.ApiService.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaekwondoOrchestration.ApiService.Controllers
 {
-    public class ApiBaseController
+    [ApiController]
+    public abstract class ApiBaseController : ControllerBase
     {
+        protected ActionResult<ApiResponse<T>> OkResponse<T>(T data) =>
+            Ok(ApiResponse<T>.Ok(data));
+
+        protected ActionResult<ApiResponse<T>> NotFoundResponse<T>(string message) =>
+            NotFound(ApiResponse<T>.Fail(message, 404));
+
+        protected ActionResult<ApiResponse<T>> CreatedResponse<T>(string routeName, object routeValues, T data) =>
+            CreatedAtAction(routeName, routeValues, ApiResponse<T>.Ok(data, 201));
     }
+
 }
