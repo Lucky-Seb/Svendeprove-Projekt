5a0880c42c2868b4652efe2092cf374392226c50
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-15 16:17:55 +0200

    Added GlobalException

--- TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Middlewares/GlobalExceptionMiddleware.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Middlewares/GlobalExceptionMiddleware.cs
@@ -2,5 +2,32 @@
 {
     public class GlobalExceptionMiddleware
     {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled Exception");
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+
+                var response = ApiResponse<string>.Fail("An unexpected error occurred.", 500);
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
     }
+
 }
