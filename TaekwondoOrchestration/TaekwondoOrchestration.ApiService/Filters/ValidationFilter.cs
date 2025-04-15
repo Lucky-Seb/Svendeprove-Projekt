97e827a5482797a5613ec1b26552231e1c044c06
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-15 16:17:10 +0200

    added ValidationFilter

--- TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Filters/ValidationFilter.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Filters/ValidationFilter.cs
@@ -1,6 +1,24 @@
-﻿namespace TaekwondoOrchestration.ApiService.Filters
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaekwondoOrchestration.ApiService.Filters
 {
-    public class ValidationFilter
+    public class ValidationFilter : IActionFilter
     {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                context.Result = new BadRequestObjectResult(ApiResponse<string>.Fail(errors, 400));
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
     }
+
 }
