30861358d7cd18e4e2c098c908fb39186e8b1c50
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-18 12:38:38 +0200

    Added Map to Pensum add update some of the others

---
+++ TaekwondoApp/TaekwondoApp.Shared/Mapping/PensumMap.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaekwondoApp.Shared.DTO;
+using TaekwondoApp.Shared.Models;
+using static TaekwondoApp.Shared.Pages.Register;
+
+namespace TaekwondoApp.Shared.Mapping
+{
+    public class PensumMap : Profile
+    {
+        public PensumMap()
+        {
+            CreateMap<Pensum, PensumDTO>();
+            CreateMap<PensumDTO, Pensum>();
+
+            CreateMap<Pensum, PensumDTO>().ReverseMap();
+            CreateMap<Quiz, QuizDTO>().ReverseMap();
+            CreateMap<Øvelse, ØvelseDTO>().ReverseMap();
+            CreateMap<Teori, TeoriDTO>().ReverseMap();
+            CreateMap<Teknik, TeknikDTO>().ReverseMap();
+        }
+    }
+}
