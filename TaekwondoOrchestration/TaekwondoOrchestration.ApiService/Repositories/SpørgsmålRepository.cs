931d4a564a80e51a7ed217ed8c181c97952b5f20
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-18 14:41:45 +0200

    updated Spørgsmål Repository

--- TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Repositories/SpørgsmålRepository.cs
+++ TaekwondoOrchestration/TaekwondoOrchestration.ApiService/Repositories/SpørgsmålRepository.cs
@@ -1,9 +1,10 @@
-﻿using TaekwondoOrchestration.ApiService.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TaekwondoOrchestration.ApiService.Data;
 using TaekwondoApp.Shared.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using TaekwondoOrchestration.ApiService.RepositorieInterfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TaekwondoOrchestration.ApiService.Repositories
 {
@@ -16,16 +17,49 @@ namespace TaekwondoOrchestration.ApiService.Repositories
             _context = context;
         }
 
+        // GET all Spørgsmål with related data
         public async Task<List<Spørgsmål>> GetAllAsync()
         {
-            return await _context.Spørgsmål.ToListAsync();
+            return await _context.Spørgsmål
+                .Include(s => s.Teknik)
+                .Include(s => s.Teori)
+                .Include(s => s.Øvelse)
+                .ToListAsync();
         }
 
+        // GET a Spørgsmål by ID with related data
         public async Task<Spørgsmål?> GetByIdAsync(Guid spørgsmålId)
         {
-            return await _context.Spørgsmål.FindAsync(spørgsmålId);
+            return await _context.Spørgsmål
+                .Include(s => s.Teknik)
+                .Include(s => s.Teori)
+                .Include(s => s.Øvelse)
+                .FirstOrDefaultAsync(s => s.SpørgsmålID == spørgsmålId);
         }
 
+        // GET a Spørgsmål by ID including deleted records (for soft deletes)
+        public async Task<Spørgsmål?> GetByIdIncludingDeletedAsync(Guid spørgsmålId)
+        {
+            return await _context.Spørgsmål
+                .IgnoreQueryFilters()  // Ignores the soft delete filter
+                .Include(s => s.Teknik)
+                .Include(s => s.Teori)
+                .Include(s => s.Øvelse)
+                .FirstOrDefaultAsync(s => s.SpørgsmålID == spørgsmålId);
+        }
+
+        // GET all Spørgsmål including deleted records (for soft deletes)
+        public async Task<List<Spørgsmål>> GetAllIncludingDeletedAsync()
+        {
+            return await _context.Spørgsmål
+                .IgnoreQueryFilters()  // Ignores the soft delete filter
+                .Include(s => s.Teknik)
+                .Include(s => s.Teori)
+                .Include(s => s.Øvelse)
+                .ToListAsync();
+        }
+
+        // CREATE a new Spørgsmål
         public async Task<Spørgsmål> CreateAsync(Spørgsmål spørgsmål)
         {
             _context.Spørgsmål.Add(spørgsmål);
@@ -33,6 +67,15 @@ namespace TaekwondoOrchestration.ApiService.Repositories
             return spørgsmål;
         }
 
+        // UPDATE an existing Spørgsmål
+        public async Task<bool> UpdateAsync(Spørgsmål spørgsmål)
+        {
+            _context.Entry(spørgsmål).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        // DELETE a Spørgsmål
         public async Task<bool> DeleteAsync(Guid spørgsmålId)
         {
             var spørgsmål = await _context.Spørgsmål.FindAsync(spørgsmålId);
@@ -44,18 +87,15 @@ namespace TaekwondoOrchestration.ApiService.Repositories
             return true;
         }
 
-        public async Task<bool> UpdateAsync(Spørgsmål spørgsmål)
-        {
-            _context.Entry(spørgsmål).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return true;
-        }
+        // GET Spørgsmål by QuizId (with related data)
         public async Task<IEnumerable<Spørgsmål>> GetByQuizIdAsync(Guid quizId)
         {
-            // Ensure that the context includes the necessary relationships (e.g., QuizID).
             return await _context.Spørgsmål
-                                 .Where(s => s.QuizID == quizId) // Filter by QuizID
-                                 .ToListAsync(); // Retrieve as a list asynchronously
+                .Where(s => s.QuizID == quizId) // Filter by QuizID
+                .Include(s => s.Teknik)
+                .Include(s => s.Teori)
+                .Include(s => s.Øvelse)
+                .ToListAsync();
         }
     }
 }
