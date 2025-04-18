799d21e24d15df4cccf18357a21b41cd45f281ef
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-18 09:29:34 +0200

    added BrugerRepositoryTests

---
+++ TaekwondoOrchestration/TaekwondoOrchestration.Tests/BrugerTests/BrugerControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.Tests.BrugerTests
+{
+    internal class BrugerControllerTests
+    {
+    }
+}
---
+++ TaekwondoOrchestration/TaekwondoOrchestration.Tests/BrugerTests/BrugerServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using Xunit;
+using FluentAssertions;
+using TaekwondoApp.Shared.Models;
+using TaekwondoApp.Shared.DTO;
+using TaekwondoOrchestration.ApiService.Repositories;
+using TaekwondoOrchestration.ApiService.Services;
+using TaekwondoOrchestration.ApiService.Helpers;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaekwondoOrchestration.Tests.BrugerTests
+{
+    internal class BrugerServiceTests
+    {
+    }
+}
