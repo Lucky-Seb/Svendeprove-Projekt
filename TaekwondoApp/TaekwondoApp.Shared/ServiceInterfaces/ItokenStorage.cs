655483db4ef1385b2281b41d413b4085b052f6fd
Lucky-Seb <Sebastian@jursrasmussen.dk>
2025-04-30 19:55:32 +0200

    problem with auth service for crossplatform in the web assamply

---
+++ TaekwondoApp/TaekwondoApp.Shared/ServiceInterfaces/ItokenStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaekwondoApp.Shared.ServiceInterfaces
+{
+    public interface ITokenStorage
+    {
+        Task SetAsync(string key, string value);
+        Task<string?> GetAsync(string key);
+        void Remove(string key);
+    }
+
+}
